32787d4b8a999e3142bb719de7b344b443015c51
rain7night <rainnight@outlook.com>
2019-05-07 17:57:01 +0800

    method dispatch

---
+++ src/main/java/method_dispatch/Dispatcher.java
@@ -0,0 +1,38 @@
+package method_dispatch;
+
+public class Dispatcher {
+    static class QQ {
+    }
+
+    static class _360 {
+    }
+
+    public static class Father {
+        public void hardChoice(QQ arg) {
+            System.out.println("father choose QQ");
+        }
+
+        public void hardChoice(_360 arg) {
+            System.out.println("father choose _360");
+        }
+    }
+
+    public static class Son extends Father {
+        @Override
+        public void hardChoice(QQ arg) {
+            System.out.println("son choose QQ");
+        }
+
+        @Override
+        public void hardChoice(_360 arg) {
+            System.out.println("son choose 360");
+        }
+    }
+
+    public static void main(String[] args) {
+        Father father = new Father();
+        Father son = new Son();
+        father.hardChoice(new _360());
+        son.hardChoice(new QQ());
+    }
+}
