b48374384968534ca7ee90cebca5f577e6e80dcc
rainnight <rainnight@outlook.com>
2019-12-04 17:51:51 +0800

    add memory Runtime print

--- src/main/java/memory/heap/MemoryUsedTest.java
+++ src/main/java/memory/heap/MemoryUsedTest.java
@@ -45,6 +45,18 @@ public class MemoryUsedTest {
     public static void main(String[] args) {
         MemoryUsedTest javaHeapTest = new MemoryUsedTest(150);
         System.out.println("Hold MB: " + javaHeapTest.getMemorySize());
+
+        Runtime run = Runtime.getRuntime();
+        long max = run.maxMemory();
+        long total = run.totalMemory();
+        long free = run.freeMemory();
+        long usable = max - total + free;
+
+        System.out.println("最大内存 = " + max);
+        System.out.println("已分配内存 = " + total);
+        System.out.println("已分配内存中的剩余空间 = " + free);
+        System.out.println("最大可用内存 = " + usable);
+
         while (true) {
             System.out.println("sleeping...");
             try {
